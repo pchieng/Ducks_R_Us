89b20dfbd4ff787c8399e795e40691234256d987
Peter Chieng <pchieng3@gmail.com>
2022-05-13 08:50:57 -0500

    Completed /products on front end

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ProductList from './products' 
 // getAPIHealth is defined in our axios-services directory index.js
 // you can think of that directory as a collection of api adapters
 // where each adapter fetches specific info from our express server's /api route
-import { getAPIHealth } from '../axios-services';
+import { getAPIHealth, getAllProducts } from '../axios-services';
 import '../style/App.css';
 
 const App = () => {
   const [APIHealth, setAPIHealth] = useState('');
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     // follow this pattern inside your useEffect calls:
@@ -17,15 +20,33 @@ const App = () => {
       setAPIHealth(healthy ? 'api is up! :D' : 'api is down :/');
     };
 
+    const getProductList = async () => {
+      const products = await getAllProducts();
+      setProducts(products);
+    }
+
     // second, after you've defined your getter above
     // invoke it immediately after its declaration, inside the useEffect callback
     getAPIStatus();
+    getProductList();
   }, []);
+
+
+
+
+
 
   return (
     <div className="app-container">
       <h1>Hello, World!</h1>
       <p>API Status: {APIHealth}</p>
+      <Router>
+        <Route path="/products">
+          <ProductList products={products} />
+        </Route>
+
+
+      </Router>
     </div>
   );
 };
