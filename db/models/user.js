d6760b68ce88debb46c13ca6e9d1c42aba351941
Turner Milan <turnermilan@gmail.com>
2022-05-13 21:53:20 -0400

    added user functions from prev branch

--- db/models/user.js
+++ db/models/user.js
@@ -1,11 +1,75 @@
 // grab our db client connection to use with our adapters
 const client = require('../client');
+const bcrypt = require("bcrypt");
+
+async function createUser({ username, password }) {
+  try {
+    const saltRounds = 10;
+    const hashedPwd = await bcrypt.hash(password, saltRounds);
+
+    const {rows: [user]} = await client.query(`
+        INSERT INTO users(
+            email,
+            username,
+            password
+        ) VALUES ($1, $2)
+        ON CONFLICT (username) DO NOTHING
+        ON CONFLICT (email) DO NOTHING
+        RETURNING *;
+        `, [username, hashedPwd]
+    );
+    delete user.password;
+    // console.log("createUser test:",user)
+    return user;
+  } catch (error) {
+
+    throw error;
+  }
+}
+
+async function getAllUsers() {
+  try {
+    const {rows} = await client.query(`
+    SELECT * 
+    FROM users
+    `,[]) 
+    return rows
+}catch(error){
+  throw error
+}
+}
+
+const getUserByUserId = async (userId) => {
+  try{
+    const { rows: [user] } = await client.query(`
+      SELECT * 
+      FROM users 
+      WHERE id = $1;
+      `, [userId]);
+    delete user.password;
+    return user;
+  }catch (error){
+  throw error
+}
+}
+
+async function getUserByUsername(username) {
+  try {
+    const {rows: [user]} = await client.query(`
+        SELECT *
+        FROM users
+        WHERE username = $1;
+        `, [username]);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
 
 module.exports = {
   // add your database adapter fns here
+  createUser,
   getAllUsers,
+  getUserByUserId,
+  getUserByUsername
 };
-
-async function getAllUsers() {
-  /* this adapter should fetch a list of users from your db */
-}
