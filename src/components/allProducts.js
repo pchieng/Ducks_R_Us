47c5943a3fe48c4b470ac3eb447a61baf3539ac7
Peter Chieng <pchieng3@gmail.com>
2022-05-21 17:48:33 -0500

    Styled all products page

--- src/components/allProducts.js
+++ src/components/allProducts.js
@@ -1,6 +1,6 @@
 /* this will allow admins to add, edit, & delete products on the front end */
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getAllProducts, deleteProduct } from '../axios-services';
 
@@ -11,27 +11,43 @@ import { getAllProducts, deleteProduct } from '../axios-services';
 
 const AllProductsList = () => {
 
-    const [products,setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
         const getProductsList = async () => {
             const products = await getAllProducts();
             setProducts(products);
-          }
-          getProductsList();
+        }
+        getProductsList();
     }, [])
 
     return (
         <>
-            <h1>PRODUCTS</h1>
+            <h1>PRODUCTS (Administrator View)</h1>
             <Link to='/allProducts/add'>
                 <button>Add New Product</button>
             </Link>
             <br />
             <div> {products.map(product =>
-                <div key={product.id}>
+                <div 
+                    key={product.id}
+                    style={{
+                        borderStyle: "solid",
+                        borderWidth: "2px",
+                        borderRadius: "5px",
+                        lineHeight: "0.3",
+                        padding: "10px",
+                        margin: "10px"
+                    }}
+                    >
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "space-evenly"
+                        }}>
                     <Link to={`/allProducts/edit/${product.id}`}>
-                    <button>Edit</button>
+                        <button>Edit</button>
                     </Link>
                     <button
                         onClick={async () => {
@@ -42,17 +58,17 @@ const AllProductsList = () => {
                             }
                         }}
                     >Delete</button>
+                    </div>
                     <h3>{`Name: ${product.name}`}</h3>
                     <p>{`Description: ${product.description}`}</p>
                     <p>{`Category: ${product.category}`}</p>
                     <p>{`Quantity: ${product.quantity}`}</p>
-                    <p>{`Price: $${product.price/100}`}</p>
+                    <p>{`Price: $${product.price / 100}`}</p>
                     {product.isActive ?
-                        <p style={{ color: "green" }}>ACTIVE</p>
+                        <p style={{ color: "green", fontWeight: "bold" }}>ACTIVE</p>
                         :
-                        <p style={{ color: "red" }}>NOT ACTIVE</p>
+                        <p style={{ color: "red", fontWeight: "bold" }}>NOT ACTIVE</p>
                     }
-                    <p>===========</p>
                 </div>
             )}
             </div>
