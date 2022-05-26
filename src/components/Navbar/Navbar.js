a1945b54a260816256a8f6f0ce06354abc821434
Jenna <itcodegirl@gmail.com>
2022-05-26 11:37:30 -0500

    Hamburger Menu added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,15 +4,19 @@ import { MenuItems } from "./MenuItems";
 import "./NavStyle.css";
 
 class Navbar extends Component {
-  render() {
+    state = { clicked: false }
+
+    render() {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">
           React<i className="fab fa-react"></i>
         </h1>
 
-        <div className="menu-icon"></div>
-
+        <div className="menu-icon" onClick={this.handleClick}>
+                {/* below is an icon. It will be one of two classes.  */}
+                <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
         <ul>
           {MenuItems.map((item, index) => {
             return (
