d64212c2833275192058a35c98f3a3b774136802
Jenna <itcodegirl@gmail.com>
2022-05-28 11:56:47 -0500

    Update

--- src/axios-services/index.js
+++ src/axios-services/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 // this file holds your frontend network request adapters
 // think about each function as a service that provides data
@@ -20,7 +20,7 @@ import axios from 'axios';
 
 export async function getAPIHealth() {
   try {
-    const { data } = await axios.get('/api/health');
+    const { data } = await axios.get("https://localhost:4000/api/health");
     return data;
   } catch (err) {
     console.error(err);
@@ -30,54 +30,56 @@ export async function getAPIHealth() {
 
 export async function getAllProducts() {
   try {
-    const { data } = await axios.get('/api/products');
+    const { data } = await axios.get("https://localhost:4000/api/products");
     return data;
   } catch (err) {
     console.error(err);
   }
 }
-
 
 export async function getAllActiveProducts() {
   try {
-    const { data } = await axios.get('/api/products/active');
+    const { data } = await axios.get(
+      "https://localhost:4000/api/products/active"
+    );
     return data;
   } catch (err) {
     console.error(err);
   }
 }
-
 
 export async function getActiveProductsByCategory(category) {
   try {
-    const {data} = await axios.get(`/api/products/category/${category}`);
+    const { data } = await axios.get(
+      `/https://localhost:4000/api/products/category/${category}`
+    );
     return data;
   } catch (err) {
     console.error(err);
   }
 }
-
 
 export async function getProductById(productId) {
   try {
-    const { data } = await axios.get(`/api/products/${productId}`);
+    const { data } = await axios.get(
+      `https://localhost:4000/api/products/${productId}`
+    );
     return data;
   } catch (err) {
     console.error(err);
   }
 }
 
-
 export async function addNewProduct(productToAdd) {
   try {
-    const { data } = await axios.post('/api/products', {
+    const { data } = await axios.post("https://localhost:4000/api/products", {
       name: productToAdd.name,
       description: productToAdd.description,
       categoryId: productToAdd.categoryId,
       quantity: productToAdd.quantity,
       price: productToAdd.price,
       isActive: productToAdd.isActive,
-      picture: productToAdd.picture
+      picture: productToAdd.picture,
     });
     return data;
   } catch (err) {
@@ -87,60 +89,59 @@ export async function addNewProduct(productToAdd) {
 
 export async function updateProduct(productId, updatedProductValues) {
   try {
-    const {data} = await axios.patch(`/api/products/${productId}`, updatedProductValues);
+    const { data } = await axios.patch(
+      `https://localhost:4000/api/products/${productId}`,
+      updatedProductValues
+    );
     return data;
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
-
 
 export async function deleteProduct(productId) {
   try {
-    const { data } = await axios.delete(`/api/products/${productId}`);
+    const { data } = await axios.delete(
+      `https://localhost:4000/api/products/${productId}`
+    );
     return data;
   } catch (error) {
     throw error;
   }
 }
-
 
 export async function getAllCategories() {
   try {
-    const { data } = await axios.get(`/api/categories/`);
+    const { data } = await axios.get(`https://localhost:4000/api/categories/`);
     return data;
   } catch (error) {
     throw error;
   }
 }
-
 
 export async function getCategoryById(categoryId) {
-  try  {
-    const { data } = await axios.get(`/api/categories/${categoryId}`);
+  try {
+    const { data } = await axios.get(
+      `https://localhost:4000/api/categories/${categoryId}`
+    );
     return data;
   } catch (error) {
     throw error;
   }
 }
-
-
-
 
 export async function getAllUsers() {
   try {
-    const { data: users } = await axios.get('/api/users');
+    const { data: users } = await axios.get("https://localhost:4000/api/users");
     return users;
   } catch (err) {
     console.error(err);
   }
 }
 
-
-
 export async function getCartProducts() {
   try {
-    const { data } = await axios.get(`/api/cart`);
+    const { data } = await axios.get(`https://localhost:4000/api/cart`);
     return data;
   } catch (error) {
     throw error;
@@ -163,14 +164,13 @@ export async function getCartProducts() {
 //   }
 // }
 
-
 export async function getAllReviews() {
   try {
-    const { data: reviews } = await axios.get('/api/reviews');
+    const { data: reviews } = await axios.get(
+      "https://localhost:4000/api/reviews"
+    );
     return reviews;
-      } catch (err) {
+  } catch (err) {
     console.error(err);
   }
 }
-
-
