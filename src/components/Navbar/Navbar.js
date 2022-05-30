34064a08c90fb7cfc801cd62859218b0a1f51037
Jenna <jenna@DESKTOP-9D2CAR7.localdomain>
2022-05-29 22:15:47 -0500

    Search bar Update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -38,4 +38,13 @@ class Navbar extends Component {
   }
 }
 
+// Search bar 
+
+{/* <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+  <input type="text" placeholder="Search.."/>
+</div> */}
+  
 export default Navbar;
